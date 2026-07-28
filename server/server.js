a484c0ae17bb13ec863cfcdf939fa173f1dd8a4c
IELTS Admin <admin@ieltsplatform.com>
2026-07-28 11:44:28 +0600

    Fix static tests directory serving path

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Initialize Database connection
 let db;
